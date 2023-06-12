9da2e05bbcee4d3712811c85c51ac6294b23adba
Ishaq Ibrahim <ishaq.ibrahim@convertdigital.com.au>
2023-06-12 15:49:02 +0500

    initial commit

---
+++ Component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const name = "Ishaq";
+
+const Component = () => {
+  return <div className="new-style">Hello {name}</div>;
+};
---
+++ analyzer.ts
@@ -0,0 +1,32 @@
+import { Project, SyntaxKind } from "ts-morph";
+
+const project = new Project({
+  tsConfigFilePath: "tsconfig.json",
+  skipAddingFilesFromTsConfig: true,
+});
+
+project.addSourceFileAtPath("Component.tsx");
+
+const sourceFile = project.getSourceFile("Component.tsx");
+
+for (const JSXElement of sourceFile.getDescendantsOfKind(
+  SyntaxKind.JsxElement
+)) {
+  JSXElement.forEachDescendant((descendant, traversal) => {
+    if (descendant.getKind() == SyntaxKind.JsxAttribute) {
+      for (const identifier of descendant.getDescendantsOfKind(
+        SyntaxKind.Identifier
+      )) {
+        console.log(identifier.getText());
+      }
+      for (const value of descendant.getDescendantsOfKind(
+        SyntaxKind.StringLiteral
+      )) {
+        console.log(value.getText());
+        value.setLiteralValue("new-style");
+      }
+    }
+  });
+}
+
+project.save();
